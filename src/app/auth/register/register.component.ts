e078c4e588d6677472eab59e4899cdd527f60c2e
Jatin Kumar <jatin208015@gmail.com>
2024-12-20 10:55:50 +0530

    register form validation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -6,36 +6,40 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  name: string = '';
-  email: string = '';
-  password: string = '';
-  address: string = '';
-  phoneNumber: string = '';
+  name = '';
+  email = '';
+  password = '';
+  address = '';
+  phoneNumber = '';
 
-  constructor(private authService: AuthService, private router: Router) {
-  }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  onSubmit() {
+  onSubmit(registerForm: any) {
+    if (registerForm.invalid) {
+      alert("Please fix the errors before submitting.");
+      return;
+    }
+
     const registerRequestBody: RegisterRequest = {
       name: this.name,
       email: this.email,
       password: this.password,
       address: this.address,
       phoneNumber: this.phoneNumber
-    }
+    };
 
     this.authService.register(registerRequestBody).subscribe({
       next: () => {
         alert("User created successfully");
         this.router.navigate(['/login']);
       },
-      error(error) {
-        console.error("Failed to create user reason", error)
+      error: (error) => {
+        console.error("Failed to create user. Reason:", error);
+        alert("Registration failed.");
       }
-    })
+    });
   }
-
 }
